2f9144ec3fa89bf7ee304d31df780b2b04798152
ImSpiDy <SpiDy2713@gmail.com>
2024-12-30 19:11:01 +0000

    lavender: Bringup for Pixelage Android 15
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- pixelage_lavender.mk
+++ pixelage_lavender.mk
@@ -9,7 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+PIXELAGE_BUILD="lavender"
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -17,7 +18,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_lavender
+PRODUCT_NAME := pixelage_lavender
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
